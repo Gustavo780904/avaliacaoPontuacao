dc7569b89d282a44318da4ed0d7326330c373230
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-05-15 20:01:16 -0300

    Leitura dos outros dados

--- src/avaliacao/Prova.java
+++ src/avaliacao/Prova.java
@@ -13,14 +13,24 @@ public class Prova {
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO ");
 		System.out.println();
 		
-		int compra; 
+		int compra, atraso; 
 		double ticket;
+		char formaPag;
 		
+	
 		System.out.print("Quantas compras o cliente fez no último ano? ");
 		compra = sc.nextInt();
 		System.out.print("Qual o ticket médio? ");
 		ticket = sc.nextDouble();
 		System.out.println();
+		
+		System.out.print("Quantas vezes o cliente atrasou o pagamento? ");
+		atraso = sc.nextInt();
+		System.out.print("A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)? ");
+		formaPag = sc.next().charAt(0);
+		System.out.println();
+		
+		
 		
 		sc.close();
 	}
