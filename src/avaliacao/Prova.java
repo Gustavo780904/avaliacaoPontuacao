c9fd6b4ea353d70887ba9d7d4327bbe533ce2fe9
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-05-15 19:45:51 -0300

    Cabecalho

---
+++ src/avaliacao/Prova.java
@@ -0,0 +1,17 @@
+package avaliacao;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Prova {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE ");
+		System.out.println("-------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO ");
+		
+		sc.close();
+	}
+}
